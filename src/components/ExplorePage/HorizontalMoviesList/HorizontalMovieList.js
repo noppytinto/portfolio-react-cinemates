f26bf1f65f2d30f438ca71ef611d746379e4e9d8
mirror42 <34626569+noppytinto@users.noreply.github.com>
2022-04-19 12:46:37 +0200

    fix nested scroll behavior

--- src/components/ExplorePage/HorizontalMoviesList/HorizontalMovieList.js
+++ src/components/ExplorePage/HorizontalMoviesList/HorizontalMovieList.js
@@ -4,27 +4,33 @@ import ListHeader from './ListHeader/ListHeader';
 
 // import * as assetsManager from '../../../utils/AssetsManager';
 
-function HorizontalMovieList({title = 'Header', movieList = [], buttonText = 'Button'}) {
+function HorizontalMovieList({
+                                 title = 'Header',
+                                 movieList = [],
+                                 buttonText = 'Button'
+                             }) {
 
-  function spawnMoviePoster(movie) {
+    function spawnMoviePoster(movie) {
+        return (
+            <li key={movie.id}>
+                <MoviePoster posterImageUrl={movie.posterUrl}
+                             alt={movie.title}
+                             movieTitle={movie.title}/>
+            </li>
+        );
+    }
+
+    ///////////////////////////////
+    // JSX
+    ///////////////////////////////
     return (
-      <li key={movie.id}>
-        <MoviePoster posterImageUrl={movie.posterUrl}
-                     alt={movie.title}
-                     movieTitle={movie.title}/>
-      </li>
+        <div className={styles['horizontal-movie-list']}>
+            <ListHeader title={title} buttonText={buttonText}/>
+            <div className={styles['list-container']}>
+                <ul>{movieList.map(movie => spawnMoviePoster(movie))}</ul>
+            </div>
+        </div>
     );
-  }
-
-  ///////////////////////////////
-  // JSX
-  ///////////////////////////////
-  return (
-    <div className={styles['horizontal-movie-list']}>
-      <ListHeader title={title} buttonText={buttonText}/>
-      <ul>{movieList.map(movie => spawnMoviePoster(movie))}</ul>
-    </div>
-  );
 }
 
 export default HorizontalMovieList;
