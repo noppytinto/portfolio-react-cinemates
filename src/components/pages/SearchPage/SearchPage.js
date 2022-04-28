2f8387f48b2ead96afafbc495cf8453685620409
noppytinto <34626569+noppytinto@users.noreply.github.com>
2022-04-28 23:56:53 +0200

    bugfixes

--- src/components/pages/SearchPage/SearchPage.js
+++ src/components/pages/SearchPage/SearchPage.js
@@ -1,18 +1,35 @@
+import { useState } from 'react';
 import styles from './SearchPage.module.css';
-import PlaceholderPage from "../../reusable/PlaceholderPage/PlaceholderPage";
 
 function SearchPage() {
+    let classes = `${styles['search-page']} `;
+    let classesSearchBox = `${styles['search-page__box']} `;
+    const [searchQuery, setSearchQuery] = useState('');
+    
+
+    ////////////////////////////
+    // FUNCTIONS
+    ////////////////////////////
+    function onSubmitHandler(ev) {
+        ev.preventdefault();
+    }
+    
 
     ////////////////////////////
     // JSX
     ////////////////////////////
     return (
-        <div className={`${styles['search-page']}`}>
-            <PlaceholderPage>
-                <p>Search page</p>
-            </PlaceholderPage>
+        <div className={classes}>
+            <p>search result for: {searchQuery}</p>
+
+            <div className={classesSearchBox}>
+                <form onSubmit={onSubmitHandler}>
+                    <input type={'search'} 
+                           placeholder={'search...'}></input>
+                </form>
+            </div>
         </div>
     );
-}
+}// SearchPage
 
 export default SearchPage;
