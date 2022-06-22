fa73d432ec5ee2433fa7239272dde51cd3492c4a
noppytinto <34626569+noppytinto@users.noreply.github.com>
2022-06-22 11:06:31 +0200

    improve list rendering

--- src/components/reusable/ListButton/ListButton.js
+++ src/components/reusable/ListButton/ListButton.js
@@ -2,14 +2,17 @@ import styles from './ListButton.module.scss';
 import { v4 as uuidv4 } from 'uuid';
 import MoviePoster from './MoviePoster/MoviePoster';
 import {useNavigate} from "react-router-dom";
+import React from 'react';
 
 
 function ListButton(props) {
+    console.log('list button rendered');
     const movieIds = props.movies ?? [];
     const listName = props.listName ?? '';
     const title = props.title ?? '';
     const titleColor = props.titleColor ?? '#000';
     const ids = getFirstNItems(movieIds, 4);
+    
     const navigate = useNavigate();
 
 
@@ -55,4 +58,17 @@ function ListButton(props) {
     );
 }// ListButton
 
-export default ListButton;
+
+function _propsAreEqual(prev, next) {
+    const prevArray = prev.movieIds ?? [];
+    const nextArray = next.movieIds ?? [];
+
+    const arraysAreEqual = 
+        prevArray.length === nextArray.length && 
+        prevArray.every((value, index) => { return value === nextArray[index]});
+
+    return arraysAreEqual;
+}
+
+export default React.memo(ListButton, _propsAreEqual);
+// export default ListButton;
